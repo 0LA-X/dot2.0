e4b16fb0a19f1f8141c473dce2b46428c0a9a64f
0LA-X <165085183+0LA-X@users.noreply.github.com>
2025-09-01 00:00:15 +0100

    Feat: added nvim config -  NVchad

---
+++ config/.config/nvim/lua/options.lua
@@ -0,0 +1,8 @@
+require "nvchad.options"
+
+-- add yours here!
+
+-- local o = vim.o
+-- o.cursorlineopt ='both' -- to enable cursorline!
+vim.opt.number = true         -- Show absolute line number
+vim.opt.relativenumber = true -- Show relative numbers
